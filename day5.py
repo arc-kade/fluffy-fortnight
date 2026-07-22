43579ee92859e82dd7554707db16e50c3d883292
Abilash <abilash.t.venu@gmail.com>
2026-07-22 22:24:28 +0530

    Day 5 assignment

---
+++ day5.py
@@ -0,0 +1,29 @@
+# 1        What type of programming language would you use? (Low-level / High-level)
+# Ans.     Low-level programming language is what programmers use.
+
+# 2        What is difference between compiler and interpreter?
+# Ans.     A compiler compiles the entire code at once and translate it to machine code.
+#          An interpreter translates each line of a program one by one into machine code.
+
+# 3        Write a short note explaining the role of:
+
+# •        Source code
+#Ans.      A source code is the original lust of instructions written by the programmer using high level language.           
+#          It is written using text editors or integrated development environments
+
+# •        Object code
+#          An object code is the low-level binary representation of your program generated automatically when the compiler processes your code.
+#          It is formatted into raw binary instruction blocks of 0s and 1s.
+
+# •        Executable file
+#          An excecutable file is the finalized software package ready for immediate computer excecution.
+#          It is built by a linker utility program.
+#          It merges multiple object files with the operating system libraries.
+# 4     List all SDLC phases and for each phase, write:        
+
+# •        What will you do in that phase?
+#          The following are the phases of Software Development Life Cycle:
+#          Planning: This is where we define the project scope, goals, estimated costs, and resource requirements.Development teams analyze technical and operational feasability and gather detailed functional and non-functional requirements.
+#          Design: The software architects and designers translate the planning phase into a comprehensive technical blueprint.
+#          Testing: Quality assurance teams and developers rigorously test the software to identify and fix any defects, bugs or vulnerabilites. This includes unit testing, integration testing and User Acceptance testing.
+#          Maintenance: Post-deployment, the development team provides ongoing support. This stage involves monitoring system performance, resolving newly discovered bugs, and rolling out updates, patches, and new features to enhance user experience.
