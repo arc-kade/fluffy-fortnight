e1ba955d6191a02724cfaa380e2dce1a267de537
Abilash <abilash.t.venu@gmail.com>
2026-07-26 22:06:13 +0530

    day10 task

--- day10.py
+++ day10.py
@@ -1,13 +1,13 @@
-# def digitalRoot(n):
-#         while n >= 10:
-#                 sum = 0
-#                 while n > 0:
-#                         sum += n%10
-#                         n //= 10
-#                 n = sum
-#         return n
-# a = int(input("Enter a any number (Digital root): "))
-# print(f"The sum of digits from the number {a} till it is single digit is {digitalRoot(a)}")
+def digitalRoot(n):
+        while n >= 10:
+                sum = 0
+                while n > 0:
+                        sum += n%10
+                        n //= 10
+                n = sum
+        return n
+a = int(input("Enter a any number (Digital root): "))
+print(f"The sum of digits from the number {a} till it is single digit is {digitalRoot(a)}")
 print()
 
 
