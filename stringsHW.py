cfe38128c48d11e5227108c9ca3b394d743991bd
Abilash <abilash.t.venu@gmail.com>
2026-08-02 20:25:50 +0530

    String homework

---
+++ stringsHW.py
@@ -0,0 +1,33 @@
+# hello = "Hello"
+# python = "Python"
+# print(hello +" "+python)
+# print("hi "*5)
+
+# any=input("Enter any string (to find it's length): ")
+# print(len(any))
+
+# som="Python is easy to learn"
+# if "Python" in som:
+#     print("the string contains \"Python\"")
+
+# cas=input("Enter any string: ")
+# print(cas.upper())
+# print(cas.lower())
+# print(cas.title())
+# print(cas.capitalize())
+# print(cas.swapcase())
+
+# welcome=" welcome to python "
+# print(welcome.lstrip())
+# print(welcome.rstrip())
+# print(welcome.strip())
+
+# text="I love Python, Python is awesome"
+# print(text.find("Python"))
+# print(text.replace("Python", "Java"))
+
+# text=input("input a string to slpit and rejoin: ")
+# separated=text.split()
+# print(separated)
+# rejoined="-".join(separated)
+# print(rejoined)
