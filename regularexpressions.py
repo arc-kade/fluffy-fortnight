30615e04ba0e2176f25d7bcad0dfa3d728fdf929
Abilash Venu <abilash.t.venu@gmail.com>
2026-08-14 15:30:33 +0530

    make a food delivery system by monday

--- regularexpressions.py
+++ regularexpressions.py
@@ -31,3 +31,20 @@ import re
 text = "python_123"
 result=re.findall(r"[a-zA-z0-9_]",text)
 print(result)
+
+import re
+emails=[]
+while True:
+    mailpattern = r"^[A-Za-z.!/%&+-\=?^~]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+    email = input("Create an email address: ")
+    if email.lower() == "no":
+        break
+    verification = re.fullmatch(mailpattern,email)
+    if bool(verification)==True:
+        if email in emails:
+            print("Email already exists")
+        else:
+            print("Email address successfully created")
+            emails.append(email)
+    else:
+        print("Invalid name for email address")
