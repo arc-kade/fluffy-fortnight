9951253663a303f53f84e03423b92b57cd25afd0
Abilash Venu <abilash.t.venu@gmail.com>
2026-09-02 14:54:03 +0530

    Learn the difference in control statements between Python and JS

---
+++ HTML/new.js
@@ -0,0 +1,25 @@
+// let name="Abilash"
+// console.log(name)
+// // const country = "Spain"
+// // country = "Peru"
+// console.log(typeof name)
+// let age=23.6
+// console.log(typeof age)
+// let student = true
+// console.log(typeof student)
+// let phnumber = null
+// console.log(typeof phnumber)
+// let address
+// address="Kochi"
+// console.log(address)
+// let a =5 
+// let b = 10
+// let sum = a+b
+// let difference = a-b
+// console.log(sum)
+// console.log(5==="5")
+let age = 25
+let id=true
+// console.log(age >=18 && id===true)
+// console.log(age >=18 || id===false)
+console.log(`my age is ${age}`)
