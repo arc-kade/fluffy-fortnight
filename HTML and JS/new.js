2d14f0412298be6aba64cd29250e2e358cece08c
Abilash Venu <abilash.t.venu@gmail.com>
2026-09-03 14:44:17 +0530

    js questions and bootstrap web page

---
+++ HTML and JS/new.js
@@ -0,0 +1,87 @@
+// let name="Abilash"
+// console.log(name)
+// // const country = "Spain"
+// // country = "Peru"
+// console.log(typeof name)
+// let age=23.6
+// console.log(typeof age)
+// let student = true
+// console.log(typeof student)
+// let phnumber = null
+// console.log(typeof phnumber)
+// let address
+// address="Kochi"
+// console.log(address)
+// let a =5 
+// let b = 10
+// let sum = a+b
+// let difference = a-b
+// console.log(sum)
+// console.log(5==="5")
+// let age = 25
+// let id=true
+// console.log(age >=18 && id===true)
+// console.log(age >=18 || id===false)
+// console.log(`my age is ${age}`)
+// install node.js and code runner
+// age = 20
+// if (age >=20){
+//     console.log("Eligible")
+// }
+// else{
+//     console.log("Ineligible")
+// }
+// let number = 0
+// if (number>0){
+//     console.log("Number is positive")
+// }
+// else if(number < 0){
+//     console.log("Number is negative")
+// }
+// else{
+//     console.log("number is 0")
+// }
+// let day = 3
+// switch(day){
+//     case 1:
+//         console.log("Monday")
+//         break
+//     case 2:
+//         console.log("Tuesday")
+//         break
+//     case 3:
+//         console.log("Wednesday")
+//         break
+//     default:
+//         console.log("Error")
+//         break
+// }
+// for(let i=0;i<=5;i++){
+//     console.log(i)
+// }
+// let i=0
+// while(i<5){
+//     console.log(i)
+//     i++
+// }
+// let i=0
+// do{
+//     console.log(i)
+//     i++
+// }while(i<0)
+// for(let i=0;i<=5;i++){
+//     if(i===3){
+//         continue
+//     }
+//     console.log(i)
+// }
+// function add(a,b){
+//     console.log(a+b)
+// }
+// add(20,30)
+const add=(a,b)=> a+b
+console.log(add(3,4))
+const greet= name=>`Hello ${name}`
+console.log(greet("Abilash"))
+let name=prompt("Enter your name: ")
+console.log(name)
---
+++ HTML and JS/tempCodeRunnerFile.js
@@ -0,0 +1,19 @@
+let name="Abilash"
+// console.log(name)
+// // const country = "Spain"
+// // country = "Peru"
+// console.log(typeof name)
+// let age=23.6
+// console.log(typeof age)
+// let student = true
+// console.log(typeof student)
+// let phnumber = null
+// console.log(typeof phnumber)
+// let address
+// address="Kochi"
+// console.log(address)
+// let a =5 
+// let b = 10
+// let sum = a+b
+// let difference = a-b
+// console.log(sum)
