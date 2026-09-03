c0bd0edb8b70ed7d90fb74f2801bc001cca08bb9
Abilash Venu <abilash.t.venu@gmail.com>
2026-09-03 15:22:38 +0530

    Questions in whatsapp

---
+++ HTML and JS/questions.js
@@ -0,0 +1,41 @@
+let number = 2
+if(number>0){
+    console.log('Number is positive')
+}
+else{
+    console.log("Not positive")
+}
+let age = 18
+if(age>=18){
+    console.log("Eligible to vote")
+}
+else{
+    console.log("Ineligible to vote")
+}
+let marks = 60
+if(marks >=40){
+    console.log("Pass")
+}
+else{
+    console.log("Fail")
+}
+let temperature = 36
+if(temperature>=35){
+    console.log("High temperature")
+}
+else{
+    console.log("Low to moderate temperature")
+}
+marks = 75
+if(marks>90){
+    console.log("Grade: A")
+}
+else if(marks >=75){
+    console.log("Grade: B")
+}
+else if(marks >= 55){
+    console.log("Grade C")
+}
+else{
+    console.log("Grade: F")
+}
