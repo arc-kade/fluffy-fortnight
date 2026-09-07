c203c47a566db6863ed0c249c1b80109905c55d8
Abilash <abilash.t.venu@gmail.com>
2026-09-08 02:10:36 +0530

    need to finish

---
+++ HTML/hw.js
@@ -0,0 +1,7 @@
+let numbers = [1,2,4,5,67]
+for (i = 0;i < numbers.length;i++){
+  console.log(numbers[i]);
+}
+let students = ["Aarav", "Priya", "Rohan", "Ananya", "Vikram"];
+students.forEach(student=>console.log(student));
+numbers = [3,4,5,6,9,10]
