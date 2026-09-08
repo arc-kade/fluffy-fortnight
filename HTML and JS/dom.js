600e2e16ad38099bec666dd4a658ade2ef48445a
Abilash Venu <abilash.t.venu@gmail.com>
2026-09-08 15:21:14 +0530

    Implement input to button on click

---
+++ HTML and JS/dom.js
@@ -0,0 +1,39 @@
+const heading = document.getElementById("title")
+console.log(heading)
+const paragraph = document.querySelector("#p1")
+console.log(paragraph)
+const paragraph2 = document.querySelector(".box ")
+console.log(paragraph2)
+const listItems = document.querySelectorAll("li")
+// console.log(listItems)
+listItems.forEach(item => {
+    item.innerText=item.innerText.toUpperCase()
+})
+const heading1 = document.getElementById("title1")
+heading1.innerText="Welcome to JavaScript"
+const container=document.getElementById("content")
+container.innerHTML="<srtong> this is a strong text</strong>"
+const link=document.getElementById("myLink")
+link.setAttribute("href","https://google.com")
+const box = document.getElementById("mybox")
+// box.classList.add("highlight")
+box.classList.remove("box")
+
+const button=document.getElementById("addButton")
+button.addEventListener("click",()=>{
+    const newItem=document.createElement("li")
+    newItem.innerText="newItem"
+    document.querySelector("#itemList").appendChild(newItem)
+})
+const button1=document.getElementById("myButton")
+button1.addEventListener("click",()=>{
+    console.log("button clicked")
+})
+const box2=document.getElementById("box2")
+box2.addEventListener("mouseover",()=>{
+    console.log("mouse over")
+})
+const input=document.getElementById("username")
+input.addEventListener("input",(event)=>{
+    console.log(event.target.value)
+})
